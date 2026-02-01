37fc9ff00204ce8828681be1d54a4a1371b051e2
Leko Shonia <lekoshonia@gmail.com>
2026-02-01 19:52:17 +0400

    dav2

---
+++ davaleba3.js
@@ -0,0 +1,6 @@
+let number =1;
+for (let b = 3; b <= 8; b++)
+    {
+  number *= b;
+}
+console.log(number);
---
+++ davaleba6.js
@@ -0,0 +1,4 @@
+let fruits=['banana', 'apple', 'orange']
+for (let i=0; i<fruits.length; i++){
+    console.log(fruits[i]);
+}
