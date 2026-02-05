88b42e91820c4277a58add94a820aa59f61cad1a
Leko Shonia <lekoshonia@gmail.com>
2026-02-05 18:13:57 +0400

    davu8

---
+++ davu8.js
@@ -0,0 +1,10 @@
+let student = {
+  firstName: "Leko",
+  lastName: "Shonia",
+  age: 22,
+  scores: [4, 7, 5, 3, 2],
+  fullName: function () {
+    return this.firstName + " " + this.lastName;
+  }
+};
+console.log(student.firstName + " " +student.age)
